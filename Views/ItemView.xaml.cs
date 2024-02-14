79a58ba726e0ccbde895c284005aefa61b4a122b
Katherine Hambley <superdesigngirl@mac.com>
2024-02-13 21:20:56 -0600

    Changed to MainView Wired up viewmodels to views

---
+++ Views/ItemView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MyToDoMauiApp.ViewModels;
+
+namespace MyToDoMauiApp.Views;
+
+public partial class ItemView : ContentPage
+{
+	public ItemView(ItemViewModel viewModel)
+	{
+		InitializeComponent();
+		viewModel.Navigation = Navigation;
+		BindingContext = viewModel;
+	}
+}
---
+++ Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MyToDoMauiApp.ViewModels;
+
+namespace MyToDoMauiApp.Views;
+
+public partial class MainView : ContentPage
+{
+	// displays the list of to-dos
+	public MainView(MainViewModel viewModel)
+	{
+		InitializeComponent();
+		viewModel.Navigation = Navigation;
+		BindingContext = viewModel;
+	}
+}
