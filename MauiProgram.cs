cdc2116271e9b11c01c4786a6a737e038fead7ef
Katherine Hambley <superdesigngirl@mac.com>
2024-02-13 21:32:28 -0600

    Added DI

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MyToDoMauiApp.Repositories;
 
 namespace MyToDoMauiApp;
 
@@ -13,13 +14,37 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+            .RegisterServices()
+			.RegisterViewModels()
+			.RegisterViews();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
 	}
+
+    public static MauiAppBuilder RegisterServices(this MauiAppBuilder mauiAppBuilder)
+	{
+        mauiAppBuilder.Services.AddSingleton<ITodoItemRepository, TodoItemRepository>();
+
+        return mauiAppBuilder;
+    }
+
+    public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.AddTransient<ViewModels.MainViewModel>();
+        mauiAppBuilder.Services.AddTransient<ViewModels.ItemViewModel>();
+        return mauiAppBuilder;
+    }
+
+    public static MauiAppBuilder RegisterViews(this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.AddTransient<Views.MainView>();
+        mauiAppBuilder.Services.AddTransient<Views.ItemView>();
+        return mauiAppBuilder;
+    }
 }
 
