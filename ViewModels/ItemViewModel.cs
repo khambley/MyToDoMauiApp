713ea0e99ea658e0d3fd594d34ff8434687b4114
Katherine Hambley <superdesigngirl@mac.com>
2024-02-28 21:49:41 -0600

    Added new items to the list

--- ViewModels/ItemViewModel.cs
+++ ViewModels/ItemViewModel.cs
@@ -1,16 +1,34 @@
 ﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MyToDoMauiApp.Models;
 using MyToDoMauiApp.Repositories;
 
 namespace MyToDoMauiApp.ViewModels
 {
 	// represents to-do list item used to create and/or edit items.
-	public class ItemViewModel : ViewModelBase
+	public partial class ItemViewModel : ViewModelBase
 	{
 		private readonly ITodoItemRepository repository;
+
+		// replaces Fody weaver from Xamarin
+		[ObservableProperty]
+		TodoItem item;
 
 		public ItemViewModel(ITodoItemRepository repository)
 		{
 			this.repository = repository;
+			Item = new TodoItem()
+			{
+				Due = DateTime.Now.AddDays(1)
+			};
+		}
+
+		[RelayCommand]
+		public async Task SaveAsync()
+		{
+			await repository.AddOrUpdateAsync(Item);
+			await Navigation.PopAsync();
 		}
 	}
 }
