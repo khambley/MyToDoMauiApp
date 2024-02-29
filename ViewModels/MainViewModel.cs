35ca012e2133f1d2379d78afdb1774992cda34e3
Katherine Hambley <superdesigngirl@mac.com>
2024-02-28 21:23:47 -0600

    Added Add button navigation p77

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,21 +1,29 @@
 ﻿using System;
+using MyToDoMauiApp.Views;
+
 using MyToDoMauiApp.Repositories;
+using CommunityToolkit.Mvvm.Input;
 
 namespace MyToDoMauiApp.ViewModels
 {
-	public class MainViewModel : ViewModelBase
+	public partial class MainViewModel : ViewModelBase
 	{
         private readonly ITodoItemRepository repository;
+        private readonly IServiceProvider services;
 
-        public MainViewModel(ITodoItemRepository repository)
+        public MainViewModel(ITodoItemRepository repository, IServiceProvider services)
 		{
             this.repository = repository;
+            this.services = services;
             Task.Run(async () => await LoadDataAsync());
         }
 
         private async Task LoadDataAsync()
         {
         }
+
+        [RelayCommand]
+        public async Task AddItemAsync() => await Navigation.PushAsync(services.GetRequiredService<ItemView>());
     }
 }
 
