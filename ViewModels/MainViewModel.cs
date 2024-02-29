36065d9ae5f32bb5ffc68362f9c71e78f314dcba
Katherine Hambley <superdesigngirl@mac.com>
2024-02-29 15:30:39 -0600

    Added data-binding to ListView in MainView

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using MyToDoMauiApp.Views;
-
 using MyToDoMauiApp.Repositories;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using MyToDoMauiApp.Models;
 
 namespace MyToDoMauiApp.ViewModels
 {
@@ -11,14 +13,33 @@ namespace MyToDoMauiApp.ViewModels
         private readonly ITodoItemRepository repository;
         private readonly IServiceProvider services;
 
+        [ObservableProperty]
+        ObservableCollection<TodoItemViewModel>? items;
+
         public MainViewModel(ITodoItemRepository repository, IServiceProvider services)
 		{
+            repository.OnItemAdded += (sender, item) => items?.Add(CreateTodoItemViewModel(item));
+            repository.OnItemUpdated += (sender, item) => Task.Run(async () => await LoadDataAsync());
+
             this.repository = repository;
             this.services = services;
             Task.Run(async () => await LoadDataAsync());
         }
 
         private async Task LoadDataAsync()
+        {
+            var items = await repository.GetItemsAsync();
+            var itemViewModels = items.Select(i => CreateTodoItemViewModel(i));
+        }
+
+        private TodoItemViewModel CreateTodoItemViewModel(TodoItem item)
+        {
+            var itemViewModel = new TodoItemViewModel(item);
+            itemViewModel.ItemStatusChanged += ItemStatusChanged;
+            return itemViewModel;
+        }
+
+        private void ItemStatusChanged(object? sender, EventArgs e)
         {
         }
 
