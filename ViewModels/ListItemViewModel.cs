b6d67479e1a9b148fc48af924b996531fee77bf9
Katherine Hambley <superdesigngirl@mac.com>
2024-03-06 13:02:51 -0600

    Add SaveCommand function

--- ViewModels/ListItemViewModel.cs
+++ ViewModels/ListItemViewModel.cs
@@ -1,16 +1,31 @@
 ﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MyToDoMauiApp.Models;
+
 using MyToDoMauiApp.Repositories;
 
 namespace MyToDoMauiApp.ViewModels
 {
-	public class ListItemViewModel :ViewModelBase
+	public partial class ListItemViewModel : ViewModelBase
 	{
         private readonly ITodoItemRepository repository;
+
+        [ObservableProperty]
+        ToDoList listItem;
 
         public ListItemViewModel(ITodoItemRepository repository)
 		{
             this.repository = repository;
+            ListItem = new ToDoList() { DateName = DateTime.Now };
         }
-	}
+
+        [RelayCommand]
+        public async Task SaveAsync()
+        {
+            await repository.AddOrUpdateListAsync(ListItem);
+            await Navigation.PopAsync();
+        }
+    }
 }
 
